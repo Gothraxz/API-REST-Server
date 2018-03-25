7e9922cb21e7b4686179b98125d5d1ba98660896
Gothraxz <eatherius@gmail.com>
2018-03-25 14:41:21 +0200

    Add book

--- src/main/java/pl/coderslab/controllers/BookController.java
+++ src/main/java/pl/coderslab/controllers/BookController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +42,19 @@ public class BookController {
 	public Book bookDetails(@PathVariable long id) {
 		return memoryBookService.getDetails(id);
 	}
-
+	
+	@PostMapping
+	public void addBook(@RequestBody Book book) {
+		List<Book> list = memoryBookService.getList();
+		long id = 0;
+		for (Book bk : list) {
+			if (bk.getId() >= id) {
+				id = bk.getId();
+				id++;
+			}
+		}
+		book.setId(id);
+		list.add(book);
+		memoryBookService.setList(list);
+	}
 }
